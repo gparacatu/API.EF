21bac1f75137045682ab016bc373e4d896357ddd
Guilherme Paracatu <guilherme_ata@hotmail.com>
2021-11-21 13:30:55 -0300

    rm

--- API.Utilitario/DataEHora.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace AppPromotora.Api.Utilitario
-{
-    public static class DataEHora
-    {
-        public static DateTime PegaHoraBrasilia() => TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
-    }
-}
